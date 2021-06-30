acf9b6111cd0e6c94a8998abf5ef81afdecdc0db
aoji-tjut <245611640@qq.com>
2021-06-30 22:44:46 +0800

    Update 0.PR、ROC.py

--- Algorithm/0.PR、ROC.py
+++ Algorithm/0.PR、ROC.py
@@ -17,20 +17,20 @@ log_reg.fit(X_train, y_train)
 y_predict = log_reg.predict(X_test)
 print("old f1_score =", f1_score(y_test, y_predict))
 decision_score = log_reg.decision_function(X_test)  # 输入sigmod中的分数值
-precisons, recalls, thresholds = precision_recall_curve(y_test, decision_score)
+precisions, recalls, thresholds = precision_recall_curve(y_test, decision_score)
 for i in range(thresholds.shape[0]):
-    if precisons[i] == recalls[i]:
+    if precisions[i] == recalls[i]:
         y_predict = np.array(decision_score > thresholds[i], dtype=int)
         print("thresholds =", thresholds[i])
         print("new f1_score =", f1_score(y_test, y_predict))
 
 plt.figure("Threshold Precision Recall")
-plt.plot(thresholds, precisons[:-1], label="precision")  # last thresholds, precisons=1, recalls=0
+plt.plot(thresholds, precisions[:-1], label="precision")  # last thresholds, precisions=1, recalls=0
 plt.plot(thresholds, recalls[:-1], label="recall")
 plt.legend()
 
 plt.figure("PR")
-plt.plot(precisons, recalls)
+plt.plot(recalls, precisions)
 
 fprs, tprs, thresholds = roc_curve(y_test, decision_score)
 print("roc_auc_score =", roc_auc_score(y_test, decision_score))
